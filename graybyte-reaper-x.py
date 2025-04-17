96bf503aadce9a732ce1a2d98c260f6d029275b9
GRAYBYT3 <33544165+Graybyt3@users.noreply.github.com>
2025-04-17 07:17:40 +0600

    Update graybyte-reaper-x.py

--- graybyte-reaper-x.py
+++ graybyte-reaper-x.py
@@ -9,46 +9,45 @@ import sys
 try:
     init()
 except:
-    print(f"{Fore.RED}[!!] cOlOrAmA MoDuLe nOt fOuNd [!!]\n{Style.RESET_ALL}")
+    print(f"{Fore.CYAN}[!!] COLORAMA MODULE NOT FOUND [!!]\n{Style.RESET_ALL}")
 
 ascii_art = (
-    f"\n\n\n\n{Fore.RED}░█▀▀█ {Fore.GREEN}░█▀▀█ {Fore.YELLOW}─█▀▀█ {Fore.GREEN}░█──░█ 　 {Fore.MAGENTA}░█▀▀█ {Fore.CYAN}░█▀▀▀ {Fore.RED}─█▀▀█ {Fore.GREEN}░█▀▀█ {Fore.YELLOW}░█▀▀▀ {Fore.MAGENTA}░█▀▀█ {Fore.CYAN}── {Fore.RED}▀▄░▄▀{Style.RESET_ALL}\n"
-    f"{Fore.RED}░█─▄▄ {Fore.GREEN}░█▄▄▀ {Fore.YELLOW}░█▄▄█ {Fore.GREEN}░█▄▄▄█ 　 {Fore.MAGENTA}░█▄▄▀ {Fore.CYAN}░█▀▀▀ {Fore.RED}░█▄▄█ {Fore.GREEN}░█▄▄█ {Fore.YELLOW}░█▀▀▀ {Fore.MAGENTA}░█▄▄▀ {Fore.CYAN}▀▀ {Fore.RED}─░█──{Style.RESET_ALL}\n"
-    f"{Fore.RED}░█▄▄█ {Fore.GREEN}░█─░█ {Fore.YELLOW}░█─░█ {Fore.GREEN}──░█── 　 {Fore.MAGENTA}░█─░█ {Fore.CYAN}░█▄▄▄ {Fore.RED}░█─░█ {Fore.GREEN}░█─── {Fore.YELLOW}░█▄▄▄ {Fore.MAGENTA}░█─░█ {Fore.CYAN}── {Fore.RED}▄▀░▀▄{Style.RESET_ALL}\n\n\n\n"
-    f"{Fore.GREEN}\033[4m[!!] InPuT/OuTpUt dIrEcToRiEs pAtH ShOuLd bE In tHiS FoRmAt[!!]\n\n{Style.RESET_ALL}"
-    f"{Fore.RED}WiNdOwS: {Fore.GREEN}\"C:\\\\Users\\\\Administrator\\\\Desktop\\\\XX\\\\parts\" (uSe dOuBlE BaCkSlAsHeS)\n"
-    f"{Fore.RED}LiNuX :{Fore.GREEN} \"/home/yourusername/Desktop/XX/parts\" (sInGlE FoRwArD SlAsHeS)\n{Style.RESET_ALL}"
-    f"{Fore.RED}mAcOs:fUcK mAcOs UsErS,gOtO To hElL YoU RiCh fUcKiNg bAsTeRd pApPuS\n\n{Style.RESET_ALL}"
+    f"\n\n\n\n{Fore.CYAN}░█▀▀█ {Fore.GREEN}░█▀▀█ {Fore.YELLOW}─█▀▀█ {Fore.GREEN}░█──░█ 　 {Fore.MAGENTA}░█▀▀█ {Fore.CYAN}░█▀▀▀ {Fore.CYAN}─█▀▀█ {Fore.GREEN}░█▀▀█ {Fore.YELLOW}░█▀▀▀ {Fore.MAGENTA}░█▀▀█ {Fore.CYAN}── {Fore.CYAN}▀▄░▄▀{Style.RESET_ALL}\n"
+    f"{Fore.CYAN}░█─▄▄ {Fore.GREEN}░█▄▄▀ {Fore.YELLOW}░█▄▄█ {Fore.GREEN}░█▄▄▄█ 　 {Fore.MAGENTA}░█▄▄▀ {Fore.CYAN}░█▀▀▀ {Fore.CYAN}░█▄▄█ {Fore.GREEN}░█▄▄█ {Fore.YELLOW}░█▀▀▀ {Fore.MAGENTA}░█▄▄▀ {Fore.CYAN}▀▀ {Fore.CYAN}─░█──{Style.RESET_ALL}\n"
+    f"{Fore.CYAN}░█▄▄█ {Fore.GREEN}░█─░█ {Fore.YELLOW}░█─░█ {Fore.GREEN}──░█── 　 {Fore.MAGENTA}░█─░█ {Fore.CYAN}░█▄▄▄ {Fore.CYAN}░█─░█ {Fore.GREEN}░█─── {Fore.YELLOW}░█▄▄▄ {Fore.MAGENTA}░█─░█ {Fore.CYAN}── {Fore.CYAN}▄▀░▀▄{Style.RESET_ALL}\n\n\n\n"
+    f"{Fore.GREEN}[!!] INPUT/OUTPUT DIRECTORIES PATH SHOULD BE IN THIS FORMAT[!!]\n\n{Style.RESET_ALL}"
+    f"{Fore.CYAN}WINDOWS: {Fore.GREEN}\"C:\\\\USERS\\\\ADMINISTRATOR\\\\DESKTOP\\\\XX\\\\PARTS\" (USE DOUBLE BACKSLASHES)\n"
+    f"{Fore.CYAN}LINUX: {Fore.GREEN} \"/HOME/YOURUSERNAME/DESKTOP/XX/PARTS\" (SINGLE FORWARD SLASHES)\n{Style.RESET_ALL}"
+    f"{Fore.CYAN}MACOS: FUCK MACOS USERS, GOTO HELL YOU RICH FUCKING BASTARD PAPPUS\n\n{Style.RESET_ALL}"
 )
 print(ascii_art)
 
 log_ascii_art = (
     "█▀█ █▀▀ ▄▀█ █▀█ █▀▀ █▀█ ▄▄ ▀▄▀   █░░ █▀█ █▀▀ █▀\n"
     "█▀▄ ██▄ █▀█ █▀▀ ██▄ █▀▄ ░░ █░█   █▄▄ █▄█ █▄█ ▄█\n\n"
-    "gRaYbYtE cHaOsLoGgEr-dEtAiLeD lOgS\n"
-    "mY LiFe iS A LiE AnD IM LiViNg iN ThIs oNlY TrUtH -gRaYbYtE\n"
+    "GRAYBYTE CHAOSLOGGER-DETAILED LOGS\n"
+    "MY LIFE IS A LIE AND IM LIVING IN THIS ONLY TRUTH -GRAYBYTE\n"
     "-----------------------------------------------------------------------------------------------------------------------------------------------\n"
-    "fOr mOrE InFoRmAtIoN AnD HeLp\n\n"
-    "TeLeGrAm : https://t.me/rex_cc\n"
-    "fAcEbOoK : https://www.facebook.com/graybyt3\n"
-    "iNsTaGrAm : https://www.instagram.com/gray_byte\n"
-    "X : https://x.com/gray_byte\n"
+    "FOR MORE INFORMATION AND HELP\n\n"
+    "TELEGRAM: https://t.me/rex_cc\n"
+    "FACEBOOK: https://www.facebook.com/graybyt3\n"
+    "INSTRAGRAM: https://www.instagram.com/gray_byte\n"
+    "X: https://x.com/gray_byte\n"
     "-----------------------------------------------------------------------------------------------------------------------------------------------\n"
 )
 
 ##**************************************************************************
-# SeT ThE DiReCtOrY Or pAtHs hErE In tHiS FoRmAt                          *
+# SET THE DIRECTORY OR PATHS HERE IN THIS FORMAT                          *
 #                                                                         *
-# LiNuX "/home/username/Desktop/parent-dir/graybyte-reaper-x/attack/"     *
-# WiNdOwS: "C:\\Users\\Administrator\\Desktop\\graybyte-reaper-x\\attack" *
+# LINUX "/HOME/USERNAME/DESKTOP/PARENT-DIR/GRAYBYTE-REAPER-X/ATTACK/"     *
+# WINDOWS: "C:\\USERS\\ADMINISTRATOR\\DESKTOP\\GRAYBYTE-REAPER-X\\ATTACK" *
 #                                                                         *
 #**************************************************************************
 
-input_directory = "/home/graybyte/Desktop/RAW-COMPLETE-CODES/GRAYYTE-REAPER-X/attack/"
-output_directory = "/home/graybyte/Desktop/RAW-COMPLETE-CODES/GRAYYTE-REAPER-X/executed/"
+input_directory = "C:\\Users\\Administrator\\Desktop\\test\\files"
+output_directory = "C:\\Users\\Administrator\\Desktop\\test\\executed"
 
 log_directory = "ChaosLogger"
-
 
 if not os.path.exists(log_directory):
     os.makedirs(log_directory)
@@ -58,10 +57,10 @@ def log_error(error_msg):
     timestamp = datetime.now().strftime('%d-%B-%Y | %I:%M:%S %p')
     if not os.path.exists(log_directory):
         os.makedirs(log_directory)
-    plain_error_msg = error_msg.replace(f"{Fore.RED}", "").replace(f"{Style.RESET_ALL}", "")
+    plain_error_msg = error_msg.replace(f"{Fore.CYAN}", "").replace(f"{Style.RESET_ALL}", "")
     entry = (
         f"\n{plain_error_msg}\n"
-        f"eNcOuNtReD On : {timestamp}\n"
+        f"ENCOUNTECYAN ON: {timestamp}\n"
         f"-----------------------------------------------------------------------------------------------------------------------------------------------\n\n"
     )
     if not os.path.exists(error_file_path):
@@ -71,28 +70,28 @@ def log_error(error_msg):
         error_file.write(entry)
 
 if not os.path.exists(input_directory):
-    error_msg = f"{Fore.RED}[!!] iNpUt dIrEcToRy nOt fOuNd: {input_directory} [!!]\n{Style.RESET_ALL}"
+    error_msg = f"{Fore.CYAN}[!!] INPUT DIRECTORY NOT FOUND: {input_directory} [!!]\n{Style.RESET_ALL}"
     print(error_msg)
     log_error(error_msg)
-    input("pReSs eNtEr tO CoNtInUe oR CtRl+c tO ExIt...")
+    input("PRESS ENTER TO CONTINUE OR CTRL+C TO EXIT...")
 
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
-    print(f"{Fore.GREEN}[+] CrEaTiNg oUtPuT DiReCtOrY ............\nCrEaTeD: {output_directory}\n{Style.RESET_ALL}")
+    print(f"{Fore.GREEN}[+] CREATING OUTPUT DIRECTORY ............\nCREATED: {output_directory}\n{Style.RESET_ALL}")
 
 input_files = [file for file in os.listdir(input_directory) if file.endswith(".txt")]
 
 if not input_files:
-    error_msg = f"{Fore.RED}[!!] No fIlEs fOuNd iN {input_directory} [!!]\n{Style.RESET_ALL}"
+    error_msg = f"{Fore.CYAN}[!!] NO FILES FOUND IN {input_directory} [!!]\n{Style.RESET_ALL}"
     print(error_msg)
     log_error(error_msg)
-    input(f"{Fore.RED}pReSs eNtEr tO CoNtInUe oR CtRl+c tO ExIt......{Style.RESET_ALL}")
+    input(f"{Fore.CYAN}PRESS ENTER TO CONTINUE OR CTRL+C TO EXIT......{Style.RESET_ALL}")
 
 if not os.path.exists("x.py"):
-    error_msg = f"{Fore.RED}[!!] x.py pReSs eNtEr tO CoNtInUe oR CtRl+c tO ExIt...... [!!]{Style.RESET_ALL}"
+    error_msg = f"{Fore.CYAN}[!!] X.PY PRESS ENTER TO CONTINUE OR CTRL+C TO EXIT...... [!!]{Style.RESET_ALL}"
     print(error_msg)
     log_error(error_msg)
-    input(f"{Fore.RED}pReSs eNtEr ExIt......{Style.RESET_ALL}")
+    input(f"{Fore.CYAN}PRESS ENTER EXIT......{Style.RESET_ALL}")
     sys.exit(1)
 
 for input_file in input_files:
@@ -100,16 +99,16 @@ for input_file in input_files:
     start_datetime = datetime.now()
     
     time.sleep(0.5)
-    print(f"{Fore.GREEN}[+] LoAdInG FiLe ............ {input_file}{Style.RESET_ALL}")
+    print(f"{Fore.GREEN}[+] LOADING FILE ............ {input_file}{Style.RESET_ALL}")
     input_file_path = os.path.join(input_directory, input_file)
     time.sleep(0.5)
     
     try:
         with open(input_file_path, 'r') as f:
             url_count = sum(1 for line in f if line.strip())
-        print(f"{Fore.GREEN}[+] LoAdEd sUcCeSsFuLlY,RuNnInG WiTh : {input_file}:\n[+] ToTaL UrL LoAdEd : {url_count}{Style.RESET_ALL}")
+        print(f"{Fore.GREEN}[+] LOADED SUCCESSFULLY, RUNNING WITH: {input_file}:\n[+] TOTAL URL LOADED: {url_count}{Style.RESET_ALL}")
     except FileNotFoundError:
-        error_msg = f"{Fore.RED}[!!] FiLe nOt fOuNd: {input_file_path} [!!]{Style.RESET_ALL}"
+        error_msg = f"{Fore.CYAN}[!!] FILE NOT FOUND: {input_file_path} [!!]{Style.RESET_ALL}"
         print(error_msg)
         log_error(error_msg)
         continue
@@ -119,20 +118,20 @@ for input_file in input_files:
         with open(checking_file_path, 'w', encoding='utf-8') as ww:
             ww.write(log_ascii_art)
     with open(checking_file_path, 'a', encoding='utf-8') as ww:
-        ww.write(f"\nFiLeNaMe: {input_file}\n")
-        ww.write(f"FiLe tAkEn fOr eXeCuTiOn : {start_datetime.strftime('%d-%B-%Y | %I:%M:%S %p')}\n")
+        ww.write(f"\nFILENAME: {input_file}\n")
+        ww.write(f"FILE TAKEN FOR EXECUTION: {start_datetime.strftime('%d-%B-%Y | %I:%M:%S %p')}\n")
         ww.write("-----------------------------------------------------------------------------------------------------------------------------------------------\n")
     
     try:
         #*********************************************
-        # CHaNgE ThE x.py tO YoUr fIlE NaMe hErE     #
+        # CHANGE THE X.PY TO YOUR FILE NAME HERE     #
         #*********************************************
 
         process = subprocess.Popen([sys.executable, "x.py"], stdin=subprocess.PIPE, text=True)
         process.communicate(input=input_file_path)
 
     except Exception as e:
-        error_msg = f"{Fore.RED}[!!] fAiLeD To eXeCuTe x.pY FoR {input_file}\n[!!] cHeCk eNvIrOnMeNt vAlIdAtIoN\n[-] PrOcEsSiNg sToPpEd............\n{Style.RESET_ALL}"
+        error_msg = f"{Fore.CYAN}[!!] FAILED TO EXECUTE X.PY FOR {input_file}\n[!!] CHECK ENVIRONMENT VALIDATION\n[-] PROCESSING STOPPED............\n{Style.RESET_ALL}"
         print(error_msg)
         log_error(error_msg)
         break
@@ -143,7 +142,7 @@ for input_file in input_files:
     if os.path.exists(output_file_path):
         os.remove(output_file_path)
     shutil.move(input_file_path, output_directory)
-    print(f"{Fore.GREEN}\nmOvInG ExEcUtEd uRl lIsT - {input_file}\nmOvEd tO : {output_directory}\n{Style.RESET_ALL}")
+    print(f"{Fore.GREEN}\nMOVING EXECUTED URL LIST - {input_file}\nMOVED TO: {output_directory}\n{Style.RESET_ALL}")
     
     end_time = time.time()
     end_datetime = datetime.now()
@@ -154,10 +153,10 @@ for input_file in input_files:
         with open(checked_file_path, 'w', encoding='utf-8') as check_file:
             check_file.write(log_ascii_art)
     with open(checked_file_path, 'a', encoding='utf-8') as check_file:
-        check_file.write(f"\nFiLeNaMe: {input_file}\n")
-        check_file.write(f"sTaRt: {start_datetime.strftime('%d-%B-%Y | %I:%M:%S %p')}\n")
-        check_file.write(f"eNd: {end_datetime.strftime('%d-%B-%Y | %I:%M:%S %p')}\n")
-        check_file.write(f"tImE TaKeN: {time_taken:.2f} sEcOnDs | uRlS ChEcKeD: {url_count}\n")
+        check_file.write(f"FILENAME: {input_file}\n")
+        check_file.write(f"START: {start_datetime.strftime('%d-%B-%Y | %I:%M:%S %p')}\n")
+        check_file.write(f"END: {end_datetime.strftime('%d-%B-%Y | %I:%M:%S %p')}\n")
+        check_file.write(f"TIME TAKEN: {time_taken:.2f} SECONDS | URLS CHECKED: {url_count}\n")
         check_file.write("-----------------------------------------------------------------------------------------------------------------------------------------------\n")
     
-    time.sleep(3)
+    time.sleep(0.5)
